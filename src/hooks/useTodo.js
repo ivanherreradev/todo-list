5d08cd67beb3ff776fdb91808a8db5a9c054498c
Ivan Herrera <123395168+ivanherreradev@users.noreply.github.com>
2023-03-14 09:44:52 -0300

    Add dispatchs of the actions

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,8 +1,50 @@
 import { useReducer } from 'react';
 import { todoReducer } from '../Context/todoReducer';
+import { ADD__TODO, DELETE__TODO, COMPLETE__TODO, UPDATE__TODO } from '../Context/actions';
 
 export const useTodo = () => {
     const initialState = [];
 
-    const [state, dispatch] = useReducer(todoReducer, initialState, init);
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    const handleAddTodo = (todo) => {
+        const action = {
+            type: ADD__TODO,
+            payload: todo,
+        };
+
+        dispatch(action);
+    };
+
+    const handleDeleteTodo = (id) => {
+        const action = {
+            type: DELETE__TODO,
+            payload: id,
+        };
+
+        dispatch(action);
+    };
+
+    const handleUpdateTodo = (id, description) => {
+        const action = {
+            type: UPDATE__TODO,
+            payload: {
+                id,
+                description,
+            },
+        };
+
+        dispatch(action);
+    };
+
+    const handleCompleteTodo = (id) => {
+        const action = {
+            type: COMPLETE__TODO,
+            payload: id,
+        };
+
+        dispatch(action);
+    };
+
+    return { handleAddTodo, handleDeleteTodo, handleUpdateTodo, handleCompleteTodo };
 };
